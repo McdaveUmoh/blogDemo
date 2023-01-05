07748212c1d7451f6673ca6500d8fdc4807ba772
Mcdave Umoh <umohmcdave@gmail.com>
2023-01-05 23:05:56 +0100

    changed the app name

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from __future__ import print_function # In python 2.7
+#from flask_sqlalchemy import sqlalchemy
 from crypt import methods
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify,flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from datetime import datetime
@@ -28,24 +29,60 @@ def load_user(user_id):
     # print(answer.username, file=sys.stderr)
     return User.query.get(int(user_id))
 
-class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default='N/A')
-    postby = db.Column(db.String(30))
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
-    def __repr__(self):
-        return 'Blog post ' + str(self.id)
-
-class User(db.Model, UserMixin) :
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     fname = db.Column(db.String(20), nullable=False)
     lname = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
+
+class Post(db.Model):
+    
+    __tablename__ = "post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    postby = db.Column(db.String(30))
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
+    
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
+class Comment(db.Model):
+    
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'post.id', ondelete="CASCADE"), nullable=False)
+    
+    def __repr__(self):
+        return 'Comment ' + str(self.id)
+
+
+class Like(db.Model):
+    
+    __tablename__ = "like"
+    id = db.Column(db.Integer, primary_key=True)
+    #sum = db.Column(db.Integer)
+    date_created = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'post.id', ondelete="CASCADE"), nullable=False)
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -55,7 +92,7 @@ class RegisterForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField("Register")
     
-    def authenticate_username(self, username) :
+    def authenticate_username(self, username):
         existing_username = User.query.filter_by(username=username.data).first()
         
         if existing_username:
@@ -101,10 +138,15 @@ def contact():
 def about():
     return render_template('about.html')
 
-@app.route('/stories', methods=['GET'])
+@app.route('/stories', methods=['GET', 'POST'])
+@login_required
 def stories():
-    all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
-    return render_template('stories.html', posts=all_posts)
+    all_posts = Post.query.order_by(Post.date_posted).all()
+    #comment = Comment.query.filter_by(id=comment_id).first()
+    #like = Like.query.filter_by(
+        #author=current_user.id, post_id=post_id).first()
+    #posts = Post.query.all()
+    return render_template('stories.html', posts=all_posts, user=current_user, like=like)
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -125,24 +167,24 @@ def posts():
         post_content = request.form['content']
         post_author = request.form.get('author') 
         post_by = answer.username
-        new_post = BlogPost(title=post_title, content=post_content, postby=post_by, author=post_author)
+        new_post = Post(title=post_title, content=post_content, postby=post_by, author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
     else:
-        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        all_posts = Post.query.order_by(Post.date_posted).all()
         return render_template('posts.html', posts = all_posts, liveuser=userin)
  
 @app.route('/posts/delete/<int:id>') 
 def delete(id):
-    post = BlogPost.query.get_or_404(id)
+    post = Post.query.get_or_404(id)
     db.session.delete(post)
     db.session.commit()
     return redirect('/posts')
 
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
-    post = BlogPost.query.get_or_404(id)
+    post = Post.query.get_or_404(id)
     if request.method == 'POST' :
         post.title = request.form['title']
         post.author = request.form['author']
@@ -150,7 +192,61 @@ def edit(id):
         db.session.commit()
         return redirect('/posts')
     else:
-        return render_template('edit.html', post=post)    
-    
+        return render_template('edit.html', post=post)
+
+@app.route("/like-post/<post_id>", methods=['GET','POST'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(
+        author=current_user.id, post_id=post_id).first()
+
+    if not post:
+         flash('Post does not exist.', category='error')
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+
+    return redirect('/stories')
+
+@app.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect('/stories')
+
+
+@app.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect('/stories')
+
 if __name__ == "__main__":
     app.run(debug=True)
